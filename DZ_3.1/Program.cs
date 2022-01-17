8834f11eb9894591dbf8d591680fe2da97b60bfb
ВИКТОР <@TREX>
2022-01-17 09:37:53 +0300

    Добавьте файлы проекта.

---
+++ DZ_3.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DZ_3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
