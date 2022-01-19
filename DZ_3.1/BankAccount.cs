5fc25d49d0acecbe02415ba35ef48d804960b7f5
trexa81 <trex81@yandex.ru>
2022-01-19 20:37:16 +0300

    счет в банке

---
+++ DZ_3.1/BankAccount.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_3._1
+{
+    public class BankAccount
+    {
+        private long _AccountNumber;
+        private double _Balance;
+        private BankAccountType _BankAccountType;
+
+        public enum BankAccountType
+        {
+            Credit,
+            Debit
+        }
+        public double Balance
+        {
+            get
+            {
+                return Random.Double.Next
+            }
+        }
+
+        public BankAccount(long AccountNumber, double Balance, string BankAccountType)
+        {
+            _AccountNumber = AccountNumber;
+            _Balance = Balance;
+            _BankAccountType = BankAccountType;
+        }
+    }
+
+    
+}
--- DZ_3.1/Program.cs
+++ DZ_3.1/Program.cs
@@ -2,11 +2,23 @@
 
 namespace DZ_3._1
 {
-    internal class Program
+    internal static class Program
     {
         static void Main(string[] args)
         {
             // DZ2.1
+            // BankAccount AccountNumber, Balance, BankAccountType
+            var bankAccount = new BankAccount();
         }
     }
 }
+
+
+
+/* public enum Type {
+Credit,
+            Debit
+        }
+        private int _id;
+private double _balance;
+private Type _type;*/
