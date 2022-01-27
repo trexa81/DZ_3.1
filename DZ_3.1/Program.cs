43f3600dd317ffe97ed2e47c9fb601ce205a0286
trexa81 <trex81@yandex.ru>
2022-01-27 18:37:37 +0300

    class House

--- DZ_3.1/Program.cs
+++ DZ_3.1/Program.cs
@@ -8,5 +8,73 @@ namespace DZ_3._1
         {
             
         }
+
+        class House
+        {
+            //Поля
+            public int porchCount;
+            public int floorCount;
+            private int roomsOnFloor;
+            private double meterCost;
+
+            //Свойства
+            public int Porchs
+            {
+                get { return porchCount; }
+                set { porchCount = value; }
+            }
+
+            public int Floors
+            {
+                get { return floorCount; }
+                set { floorCount = value; }
+            }
+
+            public int RoomsOnFloor
+            {
+                get { return roomsOnFloor; }
+                set { roomsOnFloor = value; }
+            }
+
+            public double Cost
+            {
+                get { return meterCost; }
+                set { meterCost = value; }
+            }
+
+            //Конструкторы
+            public House()
+            {
+            }
+
+            public House(int porchCount, int floorCount, int roomsOnFloor, double cost)
+            {
+                this.porchCount = porchCount;
+                this.floorCount = floorCount;
+                this.roomsOnFloor = roomsOnFloor;
+                this.meterCost = cost;
+            }
+            //Методы
+
+            public int GetRoomsCountInPorch()
+            {
+                return roomsOnFloor * floorCount;
+            }
+
+            public int GetRoomsCountInHouse()
+            {
+                return GetRoomsCountInPorch() * porchCount;
+            }
+
+            public double HouseCost()
+            {
+                return (double)GetRoomsCountInHouse() * Cost;
+            }
+
+            public override string ToString()
+            {
+                return string.Format("Количество этажей: {0} \nКоличество подъездов: {1} \nЦена за квадратный метр: {2}", Floors, Porchs, Cost);
+            }
+        }
     }
 }
